b8bac4e4ce8cddade60d796fac9ca5df86bfc4b5
Abel Begashaw <phenomenalironfist@gmail.com>
2024-08-14 16:55:39 +0300

    created the product repository implementations skeleton code

---
+++ mobile/ecommerce_app/lib/features/product/data/repositories/product_repository_impl.dart
@@ -0,0 +1,38 @@
+import '../../../../core/error/failure.dart';
+import '../../domain/entities/product.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../domain/repositories/product_repository.dart';
+
+class ProductRepositoryImpl implements ProductRepository{
+  @override
+  Future<Either<Failure, Product>> createProduct(Product product) {
+    // TODO: implement createProduct
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, void>> deleteProduct(String id) {
+    // TODO: implement deleteProduct
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<Product>>> getAllProducts() {
+    // TODO: implement getAllProducts
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, Product>> getSingleProduct(String id) {
+    // TODO: implement getSingleProduct
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, Product>> updateProduct(Product product) {
+    // TODO: implement updateProduct
+    throw UnimplementedError();
+  }
+
+}
