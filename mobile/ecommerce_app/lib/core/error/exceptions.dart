808b9dcef7670f252512b3ec64e4f1d13e142f8e
Abel Begashaw <phenomenalironfist@gmail.com>
2024-08-22 09:32:07 +0300

    Deleted the ecommerce_app folder

--- mobile/ecommerce_app/lib/core/error/exceptions.dart
+++
@@ -1,4 +0,0 @@
-class JsonParsingException implements Exception {}
-class ServerException implements Exception {}
-class ImageException implements Exception {}
-class CacheException implements Exception {}
